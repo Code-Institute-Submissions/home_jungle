78d2dfe9dd8c8829263d7e003afb14d438e1ec49
kimkesdev <Kimboelens@gmail.com>
2021-01-23 08:58:00 +0000

    secret key

--- home_jungle/settings.py
+++ home_jungle/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'os.environ.get('SECRET_KEY', '')'
+SECRET_KEY = 'xxxxxxxxxxxx'
 
 DEBUG = 'DEVELOPMENT' in os.environ
 
